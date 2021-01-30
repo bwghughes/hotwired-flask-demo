445ae4e12b6baeea0210559bc6b56626ac9c5018
Ben Hughes <b@hghs.me>
2021-01-30 15:35:41 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hotwired Flask Demo
+
+Very simple demo of how you can use hotwired to turn your simple Flask app into an SPA.
+
+Super simple programming model.
+
+Like writing Server Side Apps in the 90's.
+
+You can read more about Hotwired [here](https://turbo.hotwire.dev/handbook/introduction).
+
+## TODO
+
++ Add SSE stuff.
---
+++ app.py
@@ -0,0 +1,38 @@
+
+import time
+import uuid
+from dataclasses import dataclass
+from flask import Flask, render_template, request
+from faker import Faker
+
+app = Flask(__name__)
+
+
+@dataclass
+class Dinger:
+	dinger_id: str
+	name: str
+	address: str
+	email: str
+
+dingers = []
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/data/')
+def data():
+	f = Faker()
+	for x in range(1, 750):
+		time.sleep(0.001)
+		d = Dinger(dinger_id=str(uuid.uuid4()), 
+				   name=f.name(), 
+				   address=f.address(), 
+				   email=f.email())
+		dingers.append(d)
+	return render_template("data.html", dingers=dingers)
+
+
+
+app.run()
